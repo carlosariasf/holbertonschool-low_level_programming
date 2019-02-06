3ca1266ade06e36d55031b8d8cd9e0ba85dad19d
carlosariasf <733@holbertonschool.com>
2019-02-06 21:50:12 +0000

    Fix 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Init function
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
