5a87d81da3d3b6c6cb14a1a0e4c712a42dc298d8
carlosariasf <733@holbertonschool.com>
2019-02-07 00:25:03 +0000

    Fix 6. Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Init function
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+printf("Size of a char: 1 byte\(s)\nSize of an int: 4 byte\(s)\nSize of a long int: 4 byte\(s)\nSize of a long long int: 8 byte\(s)\nSize of a float: 4 byte\(s)\n");
+return (0);
+}
