6e1b95f8e4fc454c6c85419516d7e78567033108
carlosariasf <733@holbertonschool.com>
2019-02-07 19:41:41 +0000

    add 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Init function
+ *
+ * Description: Longer description of the function
+ * section header: Section description
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+	putchar(n);
+	if(n != '9')
+	{
+		putchar(',');
+	}
+	putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
