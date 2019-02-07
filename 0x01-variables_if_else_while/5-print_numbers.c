298de5a72712d55cb86625f8cc30e621f65fcddc
carlosariasf <733@holbertonschool.com>
2019-02-07 18:30:41 +0000

    add 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Init function
+ *
+ * Description: Longer description of the function
+ * section header: Section description
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	int n;
+	char nl = '\n';
+
+	for (n = 0; n <= 9; n++)
+	{
+	printf("%d", n);
+	}
+	putchar(nl);
+
+	return (0);
+}
