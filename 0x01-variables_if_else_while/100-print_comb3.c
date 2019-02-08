3cee99b8aa258df59a738d1db7bc40b2b9dcb021
carlosariasf <733@holbertonschool.com>
2019-02-08 20:59:56 +0000

    Fix 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,28 +11,21 @@ int main(void)
 	int n;
 	int n2;
 	int c = 0;
-	int nc;
-	int nc2;
 
 	for (n = '0'; n <= '9'; n++)
 	{
 	for (n2 = '0'; n2 <= '9'; n2++)
 	{
-		if(n != n2 && n2 != nc && n != nc2)
+		if(n <= n2 && n != n2)
 		{
 		putchar(n);
 		putchar(n2);
 		c++;
-		if (c < 99)
+		if (c < 53)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		}
-		else
-		{
-		nc = n;
-		nc2 = n2; 
 		}
 	}
 	c++;
