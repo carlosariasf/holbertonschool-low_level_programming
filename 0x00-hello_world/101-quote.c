09cd36f7103b7c30baa29e136a320213179a51c8
carlosariasf <733@holbertonschool.com>
2019-02-09 03:39:36 +0000

    Fix 101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<unistd.h>
+#include <stdlib.h>
+/**
+ * main - Init function
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
