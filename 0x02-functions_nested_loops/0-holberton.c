7d390b78ac7c480feb1024b6a7c4a3762201cc0a
carlosariasf <733@holbertonschool.com>
2019-02-12 02:54:24 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Init function
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+
+printf("%s", "Holberton\n");
+return (0);
+}
