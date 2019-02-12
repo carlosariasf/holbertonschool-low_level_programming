ea8f586ff53baf8df3eafb97496d9a0ccb86e3f1
carlosariasf <733@holbertonschool.com>
2019-02-12 19:18:21 +0000

    6 int abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_sign - Init function
+ * @n: Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int _abs(int r)
+{
+	if (r <= 0)
+	{
+		r = r * -1;
+		return (r);
+	}else
+	{
+		r = r * 1;
+		return (r);
+	}
+
+return (0);
+}
