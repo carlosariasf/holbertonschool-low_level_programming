b43e4c0801bc321afbab3fc9aad3a2aca2b9f2eb
carlosariasf <733@holbertonschool.com>
2019-02-12 18:44:52 +0000

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_sign - Init function
+ * @n: Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+
+return (0);
+}
