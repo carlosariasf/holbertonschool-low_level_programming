d6b62322836debbd5066e49c39d7e15dc33e3fd7
carlosariasf <733@holbertonschool.com>
2019-02-12 15:41:02 +0000

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Init function
+ * a blank line
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+void print_alphabet(void)
+{
+char n;
+
+for (n = 'a'; n <= 'z'; n++)
+{
+	_putchar(n);
+}
+	_putchar('\n');
+}
