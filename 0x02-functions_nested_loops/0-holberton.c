9afdbdfca23c64ef1b255d62b4d77a0b756a1bdc
carlosariasf <733@holbertonschool.com>
2019-02-12 15:26:59 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
  * main - Init function
-(* a blank line
+ * a blank line
  * Description: Longer description of the function)?
-(* section header: Section description)*
+ * section header: Section description)*
  * Return: Description of the returned value
  */
 int main(void)
 {
-int n[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'};
-int s = sizeof(n), o;
+char n[] = "Holberton";
+int s = sizeof(n) - 1;
+int o;
 
-for(o = 0; o <= s; o++)
+for(o = 0; o < s; o++)
 {
 _putchar(n[o]);
 }
+_putchar('\n');
 
 return (0);
 }
