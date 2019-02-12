31dad8257c54270491bbfe52b7366e70c4c60ba3
carlosariasf <733@holbertonschool.com>
2019-02-12 18:04:06 +0000

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * int _islower - Init function
+ * a blank line
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int _islower(int c)
+{
+	if(c >= 97 && c < 123)
+	{
+	return (1);
+	}
+
+return (0);
+}
