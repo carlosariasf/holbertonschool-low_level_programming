f9a123b44ab14a35ef12b12b7a0d1d24df250e82
carlosariasf <733@holbertonschool.com>
2019-02-12 18:19:06 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isalpha - Init function
+ * @c: Description of parameter c
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c < 91 || c >= 97 && c < 123)
+	{
+	return (1);
+	}
+
+return (0);
+}
