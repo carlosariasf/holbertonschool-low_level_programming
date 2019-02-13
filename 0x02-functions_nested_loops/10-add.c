a6ec884f7982b89665334548b91d3c61ee6b8e1d
carlosariasf <733@holbertonschool.com>
2019-02-13 03:12:49 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_last_digit - Init function
+ * @n: Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int add(int n, int n2)
+{
+	int n3 = n + n2;
+/**	_putchar((n3 / 10) + '0');
+	_putchar((n3 % 10) + '0');
+	_putchar('\n');
+**/	return (n3);
+	
+}
