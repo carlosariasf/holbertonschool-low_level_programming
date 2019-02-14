1a8173cb0fe067b73b6936a9a592f2751736115c
carlosariasf <733@holbertonschool.com>
2019-02-14 19:16:05 +0000

    Fix 0 is upper

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -10,8 +10,8 @@ int _isupper(int c)
 {
 	if (c >= 97 && c < 123)
 	{
-	return (1);
+	return (0);
 	}
 
-return (0);
+return (1);
 }
---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * print_diagonal - Init function
+ * @n: Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+void print_triangle(int size)
+{
+int c,b,a;
+
+	if (size > 0)
+	{
+		for (c = 0; c <= size; c++)
+		{
+			if (c != 0)
+			{
+			for (b = (size - c); b > 0; b--)
+			{
+			_putchar(' ');
+			}
+			}
+			for (a = 0; a < c; a++)
+			{
+			_putchar('#');
+			}
+		if (c > 0)
+		{
+		_putchar('\n');
+		}
+		}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
