7a27f7d81bbd046d64d0500e3a6c35171c57d24c
carlosariasf <733@holbertonschool.com>
2019-02-14 17:29:39 +0000

    add 9-fizz_buzz.c

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_square - Init function
+ * @size: Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+void print_square(int size)
+{
+int c,b;
+
+	if (size > 0)
+	{
+		for (c = 0; c < size; c++)
+		{
+			for (b = 0; b < size; b++)
+			{
+			_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
