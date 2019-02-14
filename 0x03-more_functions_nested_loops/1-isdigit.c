61543b0a7b8eff2497a9072ed4364714be53b60d
carlosariasf <733@holbertonschool.com>
2019-02-14 15:18:25 +0000

    add 2-mul.c

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isupper - Init function
+ * @c: Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int _isupper(int c)
+{
+	if (c >= 48 && c < 58)
+	{
+	return (1);
+	}
+
+return (0);
+}
