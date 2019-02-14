a6d7680080fd98dbdf710acb9955939b7d867e95
carlosariasf <733@holbertonschool.com>
2019-02-14 18:11:08 +0000

    add 10-print_triangle.c

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Init function
+ * @ c : Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+int c;
+
+	for (c = 0; c <= 100; c++)
+	{
+		if((c % 3) != 0 && (c % 5) != 0)
+		{
+		printf("%d ", c);
+		}
+		else if((c % 3) == 0 && (c % 5) == 0 && c > 0)
+		{
+		printf("%s", "FizzBuzz ");
+		}
+		else if((c % 3) == 0 && c > 0)
+		{
+		printf("%s", "Fizz ");
+		}
+		else if((c % 5) == 0 && c > 0)
+		{
+		printf("%s", "Buzz ");
+		}
+	}
+	printf("%c", '\n');
+
+return (0);
+}
+
