623505f20ff7e67c8dd7af9f3d6c6d2f6eb5d106
carlosariasf <733@holbertonschool.com>
2019-02-14 15:43:54 +0000

    add 5-more_numbers.c

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - Init function
+ * @ c : Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+void print_most_numbers(void)
+{
+int c;
+
+	for (c = 0; c < 10; c++)
+	{
+	if(c != 2 && c != 4)
+	{
+	_putchar('0' + c);
+	}
+	}
+_putchar('\n');
+}
