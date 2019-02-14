621839f3bd9ebb07037721831e80753ceac1a694
carlosariasf <733@holbertonschool.com>
2019-02-14 16:06:59 +0000

    add 6-print_line.c

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * more_numbers - Init function
+ * @ c : Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+void more_numbers(void)
+{
+int c, n;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+			_putchar(((n % 100) / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
