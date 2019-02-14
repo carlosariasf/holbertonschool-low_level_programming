c921e95d69e3f888de8daea2fe1078eef170a5d7
carlosariasf <733@holbertonschool.com>
2019-02-14 15:30:47 +0000

    add 3-print_numbers.c

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - Init function
+ * @a: Description of parameter n
+ * @b: Description of parameter n
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+;
+}
