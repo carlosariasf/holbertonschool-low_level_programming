4c98e942db238f8b6f5f585cfa1db94e96ebff4e
carlosariasf <733@holbertonschool.com>
2019-02-15 04:49:09 +0000

    Fix prime

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -11,14 +11,19 @@ int main(void)
 {
 long a = 612852475143;
 long b = 2;
+int c = 0;
 
 while (b < a)
 {
 	if (a % b == 0)
 	{
+	c++;
 	a = a / b;
-	printf("%ld", a);
-	break;
+	if (c == 2)
+	{
+		printf("%ld", a);
+		break;
+	}
 	}
 b++;
 }
