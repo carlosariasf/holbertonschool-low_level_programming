51d21409e2f8630eafdb3e1c00e76654310a51a1
carlosariasf <733@holbertonschool.com>
2019-02-19 20:47:53 +0000

    Fix 4

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,12 @@
 
 void print_rev(char *s)
 {
-int a, b, c;
+int a, b;
 
 for (a = 0; s[a] != '\0'; a++)
 {
-c++;
 }
-for (b = c-1; b >= 0; b--)
+for (b = a - 1; b >= 0; b--)
 {
 	_putchar(s[b]);
 }
