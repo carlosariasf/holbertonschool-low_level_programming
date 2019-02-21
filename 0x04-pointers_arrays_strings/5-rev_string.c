aacb8a24cb334ec9851e81f72d527387ad8a5c55
carlosariasf <733@holbertonschool.com>
2019-02-21 12:57:43 +0000

    update data

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "holberton.h"
+#include <stdio.h>
 /**
  * rev_string - reset
  *
@@ -7,17 +7,20 @@
  *
  * Return : a return the size of the char.
 **/
-
 void rev_string(char *s)
 {
 int a;
-int *b;
+int b;
 
-for (a = 0; s[a] != '\0'; a++);
-
-for()
+for (a = 0; s[a] != '\0'; a++)
 {
-
+}
+for(b = 0; b <= a; b++)
+{
+int c = s[b];
+s[b] = s[a] - b;
+s[a] = c;
+printf("%s", s);
 }
 
 }
