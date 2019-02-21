1c1be87dee60b905d8576b7bdfac48f0a2a169cd
carlosariasf <733@holbertonschool.com>
2019-02-21 22:48:29 +0000

    Fix 2

--- 0x05-pointers_arrays_strings/1-strncatm.c
+++ 0x05-pointers_arrays_strings/1-strncatm.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * *_strncat - reset
  *
- * @dest: char
- * @src: char
- * @n: int
- * Return: char return the size of the char.
+ * @str: char
+ *
+ * Return : a return the size of the char.
 **/
 char *_strncat(char *dest, char *src, int n)
 {
@@ -19,16 +20,16 @@ fuente = src;
 
 for (i = 0; destino[i] != '\0'; destino++)
 ;
-for (i = 0; fuente[i] != '\0'; i++)
+for(i = 0; fuente[i] != '\0'; i++)
 {
-	if (i < n)
+	if(i < n)
 	{
 		*destino = *fuente;
 		destino++;
 		fuente++;
 	}
 }
-if (n == '\0')
+if(n == '\0')
 {
 destino--;
 }
