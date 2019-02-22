73c5c1db2231d9fda80baeb4529a908b05841ab3
carlosariasf <733@holbertonschool.com>
2019-02-22 04:54:18 +0000

    Fix 2

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -15,11 +15,16 @@ int i;
 for (i = 0; dest[i] != '\0'; dest--)
 ;
 dest++;
-for (i = 0; i <= n; i++)
+for (i = 0; i < n && *dest != '\0'; i++)
 {
 	*dest = *src;
 	dest++;
 	src++;
+}
+for(i = 0; i < n && *dest == '\0'; i++)
+{
+	*dest = '*';
+	dest++;
 }
 
 return (destino);
