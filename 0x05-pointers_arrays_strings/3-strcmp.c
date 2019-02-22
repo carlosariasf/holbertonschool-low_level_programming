75d804eb11f6c7e9506256822ff3ee984d2ae14b
carlosariasf <733@holbertonschool.com>
2019-02-22 04:59:08 +0000

    add 4-rev_array.c

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * *_strncpy - reset
+ *
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: char return the size of the char.
+**/
+char *_strncpy(char *dest, char *src, int n)
+{
+char *destino = dest;
+int i;
+
+for (i = 0; dest[i] != '\0'; dest--)
+;
+dest++;
+for (i = 0; i < n && *dest != '\0'; i++)
+{
+	*dest = *src;
+	dest++;
+	src++;
+}
+for (i = 0; i < n && *dest == '\0'; i++)
+{
+	*dest = '*';
+	dest++;
+}
+
+return (destino);
+}
