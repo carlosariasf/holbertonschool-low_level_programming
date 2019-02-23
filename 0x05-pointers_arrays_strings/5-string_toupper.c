e919215f6eb051ea871ddb91f979966ac8c5af20
carlosariasf <733@holbertonschool.com>
2019-02-23 22:08:50 +0000

    5 fix

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,25 +1,21 @@
 #include "holberton.h"
 /**
  * *string_toupper - reset
- *
- * @p: int
- *
- *
+ * @s: int
  * Return: char return the size of the char.
 **/
 char *string_toupper(char *s)
 {
 int i;
-char *b = s;
 
-for (i = 0; b[i] != '\n'; i++)
+for (i = 0; s[i] != '\n'; i++)
 {
 
-if (b[i] >= 97 && b[i] <= 122)
+if (s[i] >= 97 && s[i] <= 122)
 {
-	b[i] -= 32;
+	s[i] -= 32;
 }
 }
 
-return (b);
+return (s);
 }
