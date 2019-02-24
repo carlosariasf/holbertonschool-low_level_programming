3eebcb93afa90bbcaeb96d7f568904311642b026
carlosariasf <733@holbertonschool.com>
2019-02-24 01:26:02 +0000

    Fix  3

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,23 @@
 int _strcmp(char *s1, char *s2)
 {
 int s, ss;
+int count;
+/**int fin = 0;**/
 
 for (s = 0; s1[s] != '\0'; s++)
-;
-for (ss = 0; s2[ss] != '\0'; ss++)
-;
-if (s < ss)
 {
-	return (-15);
+for (ss = 0; ss == s && s2[ss] != '\0'; ss++)
+{
+if (s1[s] > s2[ss])
+{
+	count = s1[s] - s2[ss];
 }
-else if (s > ss)
+else
 {
-	return (15);
+	count = s1[s] - s2[ss];
+}
+}
 }
 
-return (0);
+return (count);
 }
