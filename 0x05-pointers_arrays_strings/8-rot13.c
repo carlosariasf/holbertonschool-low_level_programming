2277c586c1c50d915993135b0552c17aa20aae50
carlosariasf <733@holbertonschool.com>
2019-02-24 05:28:52 +0000

    8 rot13 final

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * *rot13 - reset
+ * @s: int
+ * Return: char return the size of the char.
+**/
+char *rot13(char *s)
+{
+int i, ii;
+char abc[26] = "ABCDEFGHIJKLMabcdefghijklm";
+char nop[26] = "NOPQRSTUVWXYZnopqrstuvwxyz";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (ii = 0; ii <= 26; ii++)
+	{
+		if (abc[ii] == s[i])
+		{
+			s[i] = nop[ii];
+		}
+		else if(nop[ii] == s[i])
+		{
+			s[i] = abc[ii];
+		}
+	}
+}
+
+return (s);
+}
