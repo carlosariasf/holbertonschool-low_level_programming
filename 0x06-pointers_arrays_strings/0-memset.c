31fecd065507001dc9e1df12f419fe27efbfe575
carlosariasf <733@holbertonschool.com>
2019-02-25 15:36:44 +0000

    Push 0 memset

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * *_memset - reset
+ * @s: int
+ * @b: int
+ * @n: int
+ * Return: char return the size of the char.
+**/
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+
+for (i = 0; i <= (int) n; i++)
+{
+
+s[i] = b;
+
+}
+
+return (s);
+}
