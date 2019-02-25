85be14036c1750005e5c08a29f25046083ce6944
carlosariasf <733@holbertonschool.com>
2019-02-25 15:47:30 +0000

    add 2-strchr.c

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * *_memcpy - reset
+ * @dest: int
+ * @src: int
+ * @n: int
+ * Return: char return the size of the char.
+**/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *dest2 = dest;
+char *src2 = src;
+int i;
+
+for (i = 0; i <= (int) n; i++)
+{
+
+*dest2 = *src2;
+dest2++;
+src2++;
+
+}
+
+return (dest);
+}
