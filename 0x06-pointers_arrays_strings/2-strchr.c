ffecd5ecd99de0e9b0476dccdba146fb39e45fed
carlosariasf <733@holbertonschool.com>
2019-02-25 16:15:26 +0000

    add 3-strspn.c

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *_strchr - reset
+ * @s: int
+ * @c: int
+ * Return: char return the size of the char.
+**/
+char *_strchr(char *s, char c)
+{
+char *dest;
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+	{
+		dest = &s[i-1];
+	}
+}
+
+return (dest);
+}
