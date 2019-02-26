e687ac390869cbe0c47d3b4dfa555d18b2ddbfa2
carlosariasf <733@holbertonschool.com>
2019-02-26 03:51:20 +0000

    Fix 9

--- 0x06-pointers_arrays_strings/9-set_string.c
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * *_strstr - reset
+ * set_string - reset
  * @s: int
- * @accept: int
- * Return: char return the size of the char.
+ * @to: int
+ * Return : char return the size of the char.
 **/
 void set_string(char **s, char *to)
 {
