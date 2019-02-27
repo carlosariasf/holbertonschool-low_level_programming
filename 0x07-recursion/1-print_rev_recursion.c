f3f71f008970b8080b2f3286f3581e8cc15d2327
carlosariasf <733@holbertonschool.com>
2019-02-27 21:40:06 +0000

    Fix 1

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -2,35 +2,11 @@
 /**
  * rev_char - reset
  * @b: *char
-
-void rev_char(char *b)
-{
-if (*b != '\n')
-{
-b--;
-_putchar(*b);
-rev_char(b);
-}
-}
-
-
- * _print_rev_recursion - reset
- * @s: *char
 **/
+
 void _print_rev_recursion(char *s)
 {
-/*if (*s != '\0')
-{
-s++;
-_print_rev_recursion(s);
-}
-else
-{
-rev_char(s);
-}
-*/
-
-if(*s)
+if (*s)
 {
  _print_rev_recursion(s + 1);
 _putchar(*s);
