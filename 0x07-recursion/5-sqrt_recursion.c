32bc0e30c7cb0beac6147e4387c149fc7fe69fe6
carlosariasf <733@holbertonschool.com>
2019-02-28 03:26:00 +0000

    add 6-is_prime_number.c

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - reset
+ * @n: int
+ * 
+ * Return: 0
+**/
+
+int _sqrt_recursion(int n)
+{
+int n2;
+if (n * n)
+{
+n2 = n - 1;
+return (n2 * _sqrt_recursion(n));
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+{
+return (-1);
+}
+}
