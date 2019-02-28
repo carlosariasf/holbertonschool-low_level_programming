c9cb4e206f3b4de8e2429bd0a23c488b515b55d5
carlosariasf <733@holbertonschool.com>
2019-02-28 04:42:22 +0000

    add 100-wildcmp.c

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * wildcmp - reset
+ * @s1: int
+ * @s2: int
+ * Return: 0
+**/
+int wildcmp(char *s1, char *s2)
+{
+if (*s)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
