927d384aba330e03a71150c0eeafbb531ab9c2a7
carlosariasf <733@holbertonschool.com>
2019-02-28 04:40:33 +0000

    add 100-wildcmp.c

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * is_palindrome - reset
+ * @s: int
+ * @ : int
+ * Return: 0
+**/
+int is_palindrome(char *s)
+{
+if (*s)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
