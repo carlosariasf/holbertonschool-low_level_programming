b867b426643769657b6dde365a30500a5ee04c83
carlosariasf <733@holbertonschool.com>
2019-03-02 21:01:55 +0000

    Fix 4

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - atoi
+ * @argc: char
+ * @argv: char
+ * Return: 0
+**/
+int main(int argc, char *argv[])
+{
+int i, sum = 0;
+
+for (i = 0; i < argc; i++)
+{
+if (!isalpha(*argv[i]))
+{
+sum += atoi(argv[i]);
+}
+else
+{
+printf("%s\n", "Error");
+return (1);
+}
+}
+printf("%d\n", sum);
+
+return (0);
+}
