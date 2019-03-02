6ef6299e38dcd7c6e88f102f33ad8ab392302030
carlosariasf <733@holbertonschool.com>
2019-03-02 19:29:54 +0000

    Fix 2

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
- * main - 
- *@argc: char
- *@argv: char
- *Return - 0
+ * main - Best
+ * @argc: char
+ * @argv: char
+ * Return: 0
 **/
 int main(int argc, char *argv[])
 {
 int i, ii;
 char *p;
 
-for(i = 0; i < argc; i++)
+for (i = 0; i < argc; i++)
 {
 p = argv[i];
-for(ii = 0; p[ii] != '\0'; p++)
+for (ii = 0; p[ii] != '\0'; p++)
 {
 _putchar(*p);
 }
