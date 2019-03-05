18651213dffc1d648bdcf668b5af2ecda7a74254
carlosariasf <733@holbertonschool.com>
2019-03-05 20:42:23 +0000

    Task 2 push

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * size_string - Array
+ * @str: Size of array
+ * Return: Size string
+**/
+int size_string(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+;
+return (i);
+}
+/**
+ * *str_concat - Array
+ * @s1: Pointer string
+ * @s2: Content of array
+ * Return: Null if size == 0, or Address of pointer if size > 0
+**/
+char *str_concat(char *s1, char *s2)
+{
+int i, ii, size1, size2, sizec;
+char *p;
+
+size1 = size_string(s1);
+size2 = size_string(s2);
+sizec = size1 + size2;
+p = malloc(sizeof(char) * sizec + 1);
+if (p == NULL)
+	return (NULL);
+for (i = 0; i < size1; i++)
+	p[i] = s1[i];
+for (ii = 0; i <= sizec; i++, ii++)
+	p[i] = s2[ii];
+return (p);
+}
