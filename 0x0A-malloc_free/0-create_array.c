baca84a564c9d1dbcd48d8283d947238b4b2439f
carlosariasf <733@holbertonschool.com>
2019-03-05 17:12:27 +0000

    Fix betty and output error in checker - task 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
- *
- *
- *
+ * *create_array - Array
+ * @size: Size of array
+ * @c: Content of array
+ * Return: Null if size == 0, or Address of pointer if size > 0
 **/
 
 char *create_array(unsigned int size, char c)
@@ -12,15 +12,15 @@ char *create_array(unsigned int size, char c)
 int i;
 char *p;
 
-if(size > 0)
+if (size > 0)
 {
 p = malloc(sizeof(c) * size);
-for(i = 0; i < (int)size; i++)
+for (i = 0; i < (int)size; i++)
 {
 p[i] = c;
 }
 
-return(p);
+return (p);
 }
 
 return (0);
