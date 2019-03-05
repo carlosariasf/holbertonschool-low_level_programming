7d2f6be037f414fcea96712e3a93a941802aa517
carlosariasf <733@holbertonschool.com>
2019-03-05 17:47:18 +0000

    Fix task 0 NULL

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -12,9 +12,9 @@ char *create_array(unsigned int size, char c)
 int i;
 char *p;
 
-if (size == 0)
-	return (NULL);
 p = malloc(sizeof(char) * size);
+if (p == NULL)
+	return (NULL);
 for (i = 0; i < (int) size; i++)
 	p[i] = c;
 return (p);
