83d4031312346597ff6f0d200f593d843e181749
carlosariasf <733@holbertonschool.com>
2019-03-05 23:20:23 +0000

    Push task 3

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * **alloc_grid - Array
+ * @width: Pointer string
+ * @height: Content of array
+ * Return: Null if size == 0, or Address of pointer if size > 0
+**/
+int **alloc_grid(int width, int height)
+{
+int i;
+int **p;
+
+if (width <= 0 || height <= 0)
+	return (NULL);
+p = calloc(width, sizeof(int));
+if (p == NULL)
+	return (NULL);
+for (i = 0; i < height; i++)
+	p[i] = calloc(width, sizeof(int));
+
+return (p);
+}
