187bf811e09338797dd04a3e1a7c566365063e9e
carlosariasf <733@holbertonschool.com>
2019-03-06 03:47:43 +0000

    Push 5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *argstostr - Array
+ * @ac: Pointer string
+ * @av: Content of array
+ * Return: Null if size == 0, or Address of pointer if size > 0
+**/
+char *argstostr(int ac, char **av)
+{
+int i = ac;
+char **p = av;
+
+if (i > 0)
+	printf("%d", i);
+
+return (*p);
+}
