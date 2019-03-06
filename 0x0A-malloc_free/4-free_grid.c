22f36535f93de0402ef446b292494a3212abb42d
carlosariasf <733@holbertonschool.com>
2019-03-06 16:12:12 +0000

    Fix 4 free grid

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -9,9 +9,9 @@
 **/
 void free_grid(int **grid, int height)
 {
-int **p = grid;
-
-if (p == grid || height > 0)
-	printf("%d", height);
-
+if (height <= 0 || grid == NULL)
+	return;
+while (height) 
+	free(grid[--height]);
+free(grid);
 }
