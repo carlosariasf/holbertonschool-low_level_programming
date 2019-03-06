7b776cc3d5ea4bf0f73cf0211f5bf161a95bc78a
carlosariasf <733@holbertonschool.com>
2019-03-06 00:31:32 +0000

    Another fix task 3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -8,19 +8,16 @@
 **/
 int **alloc_grid(int width, int height)
 {
-int max = 2147483647;
 int i;
 int **p;
 
-if (width <= 0 || height <= 0 || width >= max || height >= max)
+if (width <= 0 || height <= 0)
 	return (NULL);
 p = calloc(height, sizeof(int *));
 if (p == NULL)
 	return (NULL);
 for (i = 0; i < height; i++)
 	p[i] = calloc(width, sizeof(int));
-	if (p[i] == NULL)
-		return (NULL);
 
 return (p);
 }
