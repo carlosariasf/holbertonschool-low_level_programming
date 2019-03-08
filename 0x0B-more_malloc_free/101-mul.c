a32766891e24ffc55d41b05654687cceebd8d7c7
carlosariasf <733@holbertonschool.com>
2019-03-08 04:45:52 +0000

    Fix 101

---
+++ 0x0B-more_malloc_free/101-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Fuction
+ * @ac: char
+ * @av: char
+ * Return: char
+**/
+
+int main(int ac, char **av)
+{
+int i = 0;
+
+char *p = av[0];
+*p = 'c';
+
+if (ac > i)
+	return (0);
+
+
+return (ac);
+}
