fb6541de07a350117a25bb334ddd6a4ad4e72040
carlosariasf <733@holbertonschool.com>
2019-03-08 00:41:48 +0000

    Fix 2

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -15,14 +15,14 @@ unsigned int i;
 
 if (nmemb == 0 || size == 0)
 	return (NULL);
-p = malloc(size * nmemb + 1);
+p = malloc(size * nmemb);
 if (p == NULL)
 	return (NULL);
 for (i = 0; i < nmemb; i++)
 {
 	p[i] = 0;
 }
-p[i] = '\0';
+/**p[i] = '\0';**/
 
 return (p);
 }
