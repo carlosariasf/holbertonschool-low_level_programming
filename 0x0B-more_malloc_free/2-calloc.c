5d934a66550a2ad6b9c5578ff73f05326f6eac34
carlosariasf <733@holbertonschool.com>
2019-03-08 00:38:51 +0000

    Fix 2

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_calloc - Fuction
+ * @nmemb: char
+ * @size: char
+ * @ : unsigned int
+ * Return: char
+**/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+	return (NULL);
+p = malloc(size * nmemb + 1);
+if (p == NULL)
+	return (NULL);
+for (i = 0; i < nmemb; i++)
+{
+	p[i] = 0;
+}
+p[i] = '\0';
+
+return (p);
+}
