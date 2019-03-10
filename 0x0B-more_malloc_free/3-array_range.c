1e358cdd336f05e57861cf1cd15cb1b5287c9916
carlosariasf <733@holbertonschool.com>
2019-03-10 02:27:23 +0000

    Push 3

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -9,13 +9,24 @@
 
 int *array_range(int min, int max)
 {
+int all, i = min, j = 0;
 int *p;
 
 if (min > max)
 	return (NULL);
-p = malloc(sizeof(int) * 1);
+else
+	all = max - min + 1;
+
+p = malloc(sizeof(int) * (all + 1));
 if (p == NULL)
 	return (NULL);
+while ( i <= max)
+	{
+	p[j] = i;
+	i++;
+	j++;
+	}
+p[j] = '\0';
 
 return (p);
 }
