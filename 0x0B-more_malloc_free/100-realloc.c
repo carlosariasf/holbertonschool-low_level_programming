5232ec21d98848795e43d69a92fa5355b147c255
carlosariasf <733@holbertonschool.com>
2019-03-10 16:29:17 +0000

    push 100

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -10,14 +10,24 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+void *p;
 
-void *p = ptr;
-
-if (old_size > new_size)
-	return (NULL);
-p = malloc(sizeof(int) * 1);
-if (p == NULL)
-	return (NULL);
-
-return (ptr);
+if (new_size > old_size)
+{
+	p = malloc(new_size);
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	else if (new_size == 0)
+	{
+		free(p);
+		return (NULL);
+	}
+}
+else if (new_size == old_size)
+	return (ptr);
+free(ptr);
+return (p);
 }
