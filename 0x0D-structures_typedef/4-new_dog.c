98cb67ca773b85d8e98a54d67c684fcb016aef9a
carlosariasf <733@holbertonschool.com>
2019-03-12 16:05:06 +0000

    Fix 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -10,23 +10,29 @@
 **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
 dog_t *d;
 d = malloc(sizeof(dog_t));
 if (d == NULL)
 	return (NULL);
-if (name != NULL)
-	d->name = name;
-if (age != age)
-{
-}
-else
-{
-	d->age = age;
-}
-if (owner != NULL)
-	d->owner = owner;
 
+d->name = malloc(sizeof(char *));
+if (d -> name == NULL)
+{
+	free(d->name);
+	free(d);
+	return (NULL);
+}
+	d->name = name;
+d->age = age;
+d->owner = malloc(sizeof(char *));
+if (d ->owner == NULL )
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+	return (NULL);
+}
+	d->owner = owner;
 
 return (d);
 }
