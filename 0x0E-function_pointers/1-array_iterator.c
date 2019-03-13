fb52a5587209921b4a5fe86e8d8f97b4a06a6f0a
carlosariasf <733@holbertonschool.com>
2019-03-13 17:32:51 +0000

    Fix 1

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,4 +1,3 @@
-#include <stddef.h>
 #include "function_pointers.h"
 /**
  * array_iterator - Function
@@ -10,6 +9,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
 
-for (i = 0; i < size; i++)
-	action(array[i]);
+if (array && size != 0 && action)
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
