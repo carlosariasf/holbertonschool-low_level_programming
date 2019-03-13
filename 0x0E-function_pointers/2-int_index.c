ac0ea077511e678874158353af9ba595737bcb5b
carlosariasf <733@holbertonschool.com>
2019-03-13 18:00:06 +0000

    Fix 2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -10,15 +10,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i, j, k;
 
-if (size <= 0)
-	return (-1);
-for (i = 0; i < size; i++)
+if (array && size && cmp)
 {
-	j = cmp(array[i]);
-	if (j != 0)
-		return (i);
-	k = -1;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		j = cmp(array[i]);
+		if (j != 0)
+			return (i);
+		k = -1;
+	}
 }
-
 return (k);
 }
