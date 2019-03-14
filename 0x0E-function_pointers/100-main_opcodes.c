be2f31a1bff8c1a199e10ec6e199a11f82291ed3
carlosariasf <733@holbertonschool.com>
2019-03-14 03:18:38 +0000

    Fix 3

--- 0x0E-function_pointers/100-main_opcodes.c
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -8,9 +8,7 @@
 **/
 void main(int ac, char *av[])
 {
-if (atoi(av[1]) != 0)
-{
-if (atoi(av[1]) <= 0)
+if (atoi(av[1]) < 0)
 {
 	printf("Error\n");
 	exit(2);
@@ -19,6 +17,5 @@ else if (ac != 2)
 {
 	printf("Error\n");
 	exit(1);
-}
 }
 }
