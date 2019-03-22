65dd0a304c4f9663c9c70eba2431cc0179420e01
carlosariasf <733@holbertonschool.com>
2019-03-22 02:31:29 +0000

    Push task 2

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_list - List
+ * @h: pointer
+ *
+ * Return: Size
+**/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	new_node = malloc(sizeof(list_t));
+	if (new_node != NULL)
+	{
+	new_node->str = strdup(str);
+	new_node->next = *head;
+	new_node->len++;
+	*head = new_node;
+	return (new_node);
+	}
+
+return (NULL);
+}
