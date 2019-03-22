5deee69ac67f9fb0bb20529dfb35a3bc47597843
carlosariasf <733@holbertonschool.com>
2019-03-22 02:49:10 +0000

    Fix 2

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "lists.h"
 
+unsigned int size_string(char *str);
 /**
  * add_node - List
  * @head: pointer
@@ -13,17 +14,33 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-
+	
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node != NULL)
 	{
-	new_node->str = strdup(str);
-	new_node->next = *head;
-	new_node->len++;
-	*head = new_node;
+		if (str != NULL)
+		{
+			new_node->str = strdup(str);
+			new_node->next = *head;
+			new_node->len = size_string((char *)str);
+			*head = new_node;
+		}
 	return (new_node);
 	}
 
 return (NULL);
+}
+/**
+ * size_string - Array
+ * @str: Size of array
+ * Return: Size string
+**/
+unsigned int size_string(char *str)
+{
+unsigned int i;
+
+for (i = 0; str[i] != '\0'; i++)
+;
+return (i);
 }
