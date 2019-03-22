02855dfeddc4dd4db6fa3caedcb0761be409033c
carlosariasf <733@holbertonschool.com>
2019-03-22 04:16:53 +0000

    Push 3 and 100

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * antes - test
+**/
+void __attribute__ ((constructor)) antes()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+unsigned int size_string(char *str);
+/**
+ * add_node - List
+ * @head: pointer
+ * @str: string
+ * Return: Address of new node
+**/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *last_node = *head;
+
+	new_node = malloc(sizeof(list_t));
+	if(new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (str != NULL)
+	{
+		new_node->str = strdup(str);
+		new_node->len = size_string((char *)str);
+		new_node->next = NULL;
+	}
+	if(*head == NULL)
+	{
+		*head = new_node;
+	}
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+	last_node->next = new_node;
+
+return (new_node);
+}
+/**
+ * size_string - Array
+ * @str: Size of array
+ * Return: Size string
+**/
+unsigned int size_string(char *str)
+{
+unsigned int i;
+
+for (i = 0; str[i] != '\0'; i++)
+;
+return (i);
+}
