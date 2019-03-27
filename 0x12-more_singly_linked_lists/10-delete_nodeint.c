6b440a788e9a360624446527b8ffc3ea9b3b1952
carlosariasf <733@holbertonschool.com>
2019-03-27 03:54:19 +0000

    Push 10

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - List
+ * @head: pointer
+ * @idx: index
+ * @n: int
+ * Return: index node
+**/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *temp, *nextn;
+unsigned int i = 0;
+
+temp = *head;
+if (*head == NULL)
+	return (-1);
+if (index == 0)
+{
+	*head = (temp)->next;
+	free(temp);
+	return (1);
+}
+while (temp != NULL && i < index - 1)
+{
+	temp = temp->next;
+	i++;
+}
+if (temp == NULL || temp->next == NULL)
+	return (-1);
+nextn = temp->next->next;
+free(temp->next);
+temp->next = nextn;
+return (1);
+}
