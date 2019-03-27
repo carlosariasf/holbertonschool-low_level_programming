7bde698a4c4926b2dfcb567fa0402869bb174220
carlosariasf <733@holbertonschool.com>
2019-03-27 04:11:15 +0000

    Push 100

---
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ * reverse_listint - List
+ * @head: pointer
+ * Return: index node
+**/
+listint_t *reverse_listint(listint_t **head)
+{
+
+while (*head)
+{
+}
+
+return (*head);
+}
