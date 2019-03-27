2cff60197a75a9dbce4f7de09f706e0fded78a63
carlosariasf <733@holbertonschool.com>
2019-03-27 02:31:17 +0000

    Fix 0

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,6 @@
 #include <stdarg.h>
 #include "lists.h"
 
-int countN(const listint_t *head);
 /**
  * print_listint - List
  * @h: pointer
@@ -12,35 +11,16 @@ int countN(const listint_t *head);
 **/
 size_t print_listint(const listint_t *h)
 {
-int a = 0, b;
+int a = 0;
 const listint_t *current = h;
 
-b = countN(h);
 
-	while (a < b)
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
 		a++;
 	}
 
-return (b);
-}
-/**
- * countN - Count number of nodes
- * @head: Pointer to head of linked list
- * Return: number of nodes
-**/
-int countN(const listint_t *head)
-{
-	int count = 0;
-	const listint_t *current = head;
-
-	while (current != NULL)
-	{
-	count++;
-	current = current->next;
-	}
-
-	return (count);
+return (a);
 }
