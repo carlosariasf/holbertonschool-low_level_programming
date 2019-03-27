d04457c3296276988d72bdd6cc296264e70cf005
carlosariasf <733@holbertonschool.com>
2019-03-27 03:29:39 +0000

    Push 8

---
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ * sum_listint - List
+ * @head: pointer
+ *
+ * Return: index node
+**/
+int sum_listint(listint_t *head)
+{
+listint_t *curr = head;
+int i = 0;
+
+if(curr != NULL)
+{
+	while (curr)
+	{
+		i += curr->n;
+		curr = curr->next;
+	}
+	return (i);
+}
+
+return (0);
+}
