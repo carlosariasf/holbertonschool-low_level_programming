5718c184c8c88ce47da75ca84010c65328fcfa87
carlosariasf <733@holbertonschool.com>
2019-03-29 00:32:44 +0000

    Push 1

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * print_binary - Binary
+ * @n: int number
+ * Return: void.
+**/
+void print_binary(unsigned long int n)
+{
+unsigned long int h = n & 1;
+
+	if (n > 1)
+	{
+		n = n >> 1;
+		print_binary(n);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (h == 0)
+		_putchar('0');
+	else if (h == 1)
+		_putchar('1');
+}
