39fe9264ef83fc1f28f10fe513979773aa0d0790
carlosariasf <733@holbertonschool.com>
2019-03-29 01:47:05 +0000

    Fix 2

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * get_binary - first point.
+ * @n: pointer to a string.
+ * @index: test.
+ * Return: test
+ **/
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index < 64)
+	return (n >> index & 1);
+return (-1);
+}
