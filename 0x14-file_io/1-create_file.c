77dbfcbbddb0caa3a2634ce690441510d464372b
carlosariasf <733@holbertonschool.com>
2019-04-03 03:08:42 +0000

    Fix 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,8 +19,8 @@ if (text_content == NULL)
 	return (1);
 while (text_content[i])
 	i++;
-f = open(filename, O_TRUNC | O_CREAT | O_RDWR, 0600);
-if (f == -1)
+f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (f == -1 || !filename)
 	return (-1);
 write(f, text_content, i);
 close(f);
