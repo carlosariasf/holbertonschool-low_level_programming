ba7a0166f290b0bc2a02b2becdb25af322be6dfa
carlosariasf <733@holbertonschool.com>
2019-04-03 05:23:40 +0000

    Fix 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -14,7 +14,7 @@
  */
 int main(int ac, char **av)
 {
-int fs, fd, rr, rw;
+int fs, fd, rr, rw, c;
 char *buffer;
 
 buffer = malloc(sizeof(char) * 1024);
@@ -30,7 +30,12 @@ if (fs == -1 || rr == -1)
 	dprintf(2, "Error: Can't read from file %s\n", av[1]);
 	exit(98);
 }
-close(fs);
+c = close(fs);
+if (c == -1)
+{
+	dprintf(2, "Error: Can't close fd %s\n", av[1]);
+	exit(100);
+}
 
 fd = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 rw = write(fd, buffer, rr);
@@ -38,6 +43,12 @@ if (fd == -1 || rw == -1)
 {
 	dprintf(2, "Error: Can't write to %s\n", av[2]);
 	exit(99);
+}
+c = close(fd);
+if (c == -1) 
+{
+	dprintf(2, "Error: Can't close fd %s\n", av[2]);
+	exit(100);
 }
 
 return (0);
