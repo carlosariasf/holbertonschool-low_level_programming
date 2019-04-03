9ad74833fceb1de66177860a77cb016afd2f797c
carlosariasf <733@holbertonschool.com>
2019-04-03 03:11:24 +0000

    Fix 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,13 +15,14 @@ int create_file(const char *filename, char *text_content)
 {
 int f, i = 0;
 
+
+f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (f == -1 || !filename)
+	return (-1);
 if (text_content == NULL)
 	return (1);
 while (text_content[i])
 	i++;
-f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-if (f == -1 || !filename)
-	return (-1);
 write(f, text_content, i);
 close(f);
 
