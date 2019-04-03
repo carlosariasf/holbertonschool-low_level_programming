55aa24153ce1e650ba178ff9c48246516e7a0225
carlosariasf <733@holbertonschool.com>
2019-04-03 03:25:41 +0000

    Fix 2

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
 #include "holberton.h"
 /**
  * append_text_to_file - First
@@ -16,16 +15,14 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 int f, i = 0;
 
+f = open(filename, O_RDWR | O_APPEND);
 if (text_content == NULL)
 	return (1);
 while (text_content[i])
 	i++;
-f = open(filename, O_RDWR | O_APPEND);
-if (errno == EACCES)
+if (f == -1 || !filename)
 	return (-1);
-if (f == -1)
-	return (-1);
-write(f, text_content, (i + 1));
+write(f, text_content, i);
 close(f);
 
 return (1);
