4aa1889528d450fd5d4219d45057b3378f995fd3
carlosariasf <733@holbertonschool.com>
2019-05-07 01:53:41 +0000

    Fix script

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/carlosariasf/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/random.so
-export LD_PRELOAD=$PWD/random.so
+wget -P /tmp/ https://github.com/carlosariasf/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/random.so
+export LD_PRELOAD=/tmp/random.so
