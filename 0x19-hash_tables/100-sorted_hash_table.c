9b561343a669abfbd1b2a3b13a7e7b60757a6a3b
carlosariasf <carlosarias@icreativa.com.co>
2019-05-31 05:01:49 +0000

    Push 100

---
+++ 0x19-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,54 @@
+#include "hash_tables.h"
+/**
+ * shash_table_create - Create table
+ * @size: size of hs table
+ * Return: ne hs table
+**/
+shash_table_t *shash_table_create(unsigned long int size)
+{
+
+}
+/**
+ * shash_table_set - Create table
+ * @key: key hs
+ * @size: size of hs table
+ * Return: ne hs table
+**/
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+
+}
+/**
+ * shash_table_get - Create table
+ * @ht: key hs
+ * @key: size of hs table
+ * Return: ne hs table
+**/
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+
+}
+/**
+ * shash_table_print - Create table
+ * @ht: size of hs table
+**/
+void shash_table_print(const shash_table_t *ht)
+{
+
+}
+/**
+ * shash_table_print_rev - Create table
+ * @ht: size of hs table
+**/
+void shash_table_print_rev(const shash_table_t *ht)
+{
+
+}
+/**
+ * shash_table_delete - Create table
+ * @ht: size of hs table
+**/
+void shash_table_delete(shash_table_t *ht)
+{
+
+}
