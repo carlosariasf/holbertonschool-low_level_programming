fb9a0334b1047362823db65e2cdbfa63a2c5f37b
carlosariasf <carlosarias@icreativa.com.co>
2019-05-31 04:45:48 +0000

    Push betty task 4

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -1,18 +1,23 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_get - Get table
+ * @ht: hash table
+ * @key: key to return
+ * Return: value
+**/
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int i = 0;
-	hash_node_t *tmp; 
+	hash_node_t *tmp;
 
-	i = key_index((const unsigned char*)key, ht->size);
+	i = key_index((const`unsigned char *)key, ht->size);
 	tmp = ht->array[i];
 	while (tmp != NULL && tmp->key != NULL && strcmp(key, tmp->key) > 0)
 		tmp = tmp->next;
 	if (tmp == NULL || tmp->key == NULL || strcmp(key, tmp->key) != 0)
-		return NULL;
+		return (NULL);
 
 	else
-		return tmp->value;
+		return (tmp->value);
 }
