49cf55b16f59989037654093b558916a14c78d3d
carlosariasf <carlosarias@icreativa.com.co>
2019-05-31 04:24:01 +0000

    Push task 4

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -4,15 +4,15 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int i = 0;
-	hash_node_t **tmp; 
+	hash_node_t *tmp; 
 
-	if (ht != NULL && key != NULL)
-	{
-		for(i = 0; i < (int)ht->size; i++)
-		{
-			if ((*tmp)->key == key)
-				return (*tmp)->value;
-		}
-	}
-	return (NULL);
+	i = key_index((const unsigned char*)key, ht->size);
+	tmp = ht->array[i];
+	while (tmp != NULL && tmp->key != NULL && strcmp(key, tmp->key) > 0)
+		tmp = tmp->next;
+	if (tmp == NULL || tmp->key == NULL || strcmp(key, tmp->key) != 0)
+		return NULL;
+
+	else
+		return tmp->value;
 }
