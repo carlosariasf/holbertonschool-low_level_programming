d3f26a5540876a7bbdfcc1e413735b6d670489a3
carlosariasf <carlosarias@icreativa.com.co>
2019-05-31 19:40:02 +0000

    [FILE] Push task 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -12,15 +12,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	htable = malloc(sizeof(hash_table_t));
 	if (htable == NULL)
 		return (NULL);
-	htable->array = malloc(sizeof(hash_node_t) * size);
+	htable->array = malloc(sizeof(hash_node_t *) * size);
 	if (htable->array == NULL)
 	{
 		free(htable);
 		return (NULL);
 	}
+	htable->size = size;
 	for (i = 0; i < size; i++)
 		htable->array[i] = NULL;
-	htable->size = size;
 
 	return (htable);
 }
