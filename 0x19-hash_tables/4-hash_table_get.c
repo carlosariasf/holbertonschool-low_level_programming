f220a1867b1b1bf235cefcd71f238d3775396d4d
carlosariasf <carlosarias@icreativa.com.co>
2019-06-01 02:33:17 +0000

    [FILE] Push 4

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -5,7 +5,6 @@
  * @key: key to return
  * Return: value
 **/
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int i = 0;
@@ -13,9 +12,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	i = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[i];
+	while (tmp != NULL && tmp->key != NULL && strcmp(key, tmp->key) > 0)
+		tmp = tmp->next;
+
 	if (tmp == NULL || tmp->key == NULL || strcmp(key, tmp->key) != 0)
 		return (NULL);
-
 	else
 		return (tmp->value);
 }
