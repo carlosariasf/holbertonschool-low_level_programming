b69aae2436c818165a71e2068fb133817752f33b
carlosariasf <carlosarias@icreativa.com.co>
2019-06-17 21:28:24 +0000

    Push header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -11,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void swap_list(int *array, int pos1, int pos2);
 void print_array(const int *array, size_t size);
