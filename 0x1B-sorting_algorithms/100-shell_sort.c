03191b515da91ca3c2cd9b27108b9cab57895bed
carlosariasf <carlosarias@icreativa.com.co>
2019-06-19 23:43:30 +0000

    Push 100

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -8,32 +8,33 @@ void shell_sort(int *array, size_t size)
 {
 	int sizet = 0, n = 1, i = 0, tmp = 0, flag = 0;
 
-	if ((int)size > 1)
-		return;
-	sizet = (int) size - 1;
-	while (n < sizet / 2)
+	if (size > 1)
 	{
-		n = n * 3 + 1;
-	}
-	tmp = n;
-	for (i = 0; tmp <= sizet; i++)
-	{
-		if (array[i] > array[tmp])
+		sizet = (int) size - 1;
+		while (n < sizet / 2)
 		{
-			swap_list(array, i, tmp);
-			flag = 1;
+			n = n * 3 + 1;
 		}
-		tmp++;
-		if (flag == 1 && tmp == sizet + 1)
-			i = -1, tmp = n, flag = 0;
-		if (flag == 0 && tmp == sizet + 1)
+		tmp = n;
+		for (i = 0; tmp <= sizet; i++)
 		{
-			if (n > 1)
+			if (array[i] > array[tmp])
 			{
-				n = n / 3;
-				i = -1, tmp = n, flag = 0;
+				swap_list(array, i, tmp);
+				flag = 1;
 			}
-			print_array(array, size);
+			tmp++;
+			if (flag == 1 && tmp == sizet + 1)
+				i = -1, tmp = n, flag = 0;
+			if (flag == 0 && tmp == sizet + 1)
+			{
+				if (n > 1)
+				{
+					n = n / 3;
+					i = -1, tmp = n, flag = 0;
+				}
+				print_array(array, size);
+			}
 		}
 	}
 }
